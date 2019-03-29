3e73ae28d7131d765cc520da77bc6f4d7fd32d18
Jakub Kuryłowicz <s9989@msh.pjwstk.edu.pl>
2019-03-29 11:58:26 +0100

    second try

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TestClass tc = new TestClass();
+            var tc = new TestClass(); // var - alias
+
+            tc.go();
+
+            
+
+            var st = new TestClass
+            {
+                str = "str"
+            };
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/sub/TestClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class TestClass
+    {
+        public string str { get; set; }
+
+        public int wiek { get; set; }
+        
+        private int rok;
+
+        public int Rok
+        {
+            get { return rok; }
+            set { rok = value; }
+        }
+
+
+        public void go()
+        {
+            Console.WriteLine("TestClass");
+        }
+    }
+}
