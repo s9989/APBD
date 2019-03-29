0d8f80ac6413ebb9b47c411fbdf2c8f0b81f0e1b
Jakub Kuryłowicz <s9989@msh.pjwstk.edu.pl>
2019-03-29 12:10:06 +0100

    third try

---
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+            OkButton.Content = "Ok";
+//            Name1.Content = "Name1";
+            OkButton.Click += OkButton_Click; ;
+            Name3.Click += Name3_Click;
+            
+        }
+
+        private void Name3_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            if ((String)b.Content == "Changed")
+            {
+                b.Content = "Ok";
+            } else {
+                b.Content = "Changed";
+            }
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window w = new Window1();
+            w.ShowDialog();
+        }
+    }
+}
