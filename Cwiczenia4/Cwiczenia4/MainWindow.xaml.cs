f14aa5e6177f9aabe93019808e4967f08254922c
Jakub Kurylowicz <czterolistny@gmail.com>
2019-04-10 20:39:38 +0200

    cwiczenie4

---
+++ Cwiczenia4/Cwiczenia4/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Cwiczenia4.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cwiczenia4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowSelectedButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddStudentButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Cwiczenia4/Cwiczenia4/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenia4.Models
+{
+    public class Student
+    {
+        public int IdStudent { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+    }
+}
